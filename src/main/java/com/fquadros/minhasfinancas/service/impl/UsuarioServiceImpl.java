cba63924b405b0a915b9d96083177ec7a4529614
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-04-19 20:04:05 -0300

    create autenticar method

--- src/main/java/com/fquadros/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/fquadros/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -1,11 +1,15 @@
 package com.fquadros.minhasfinancas.service.impl;
 
+import com.fquadros.minhasfinancas.exception.ErroAutenticacao;
 import com.fquadros.minhasfinancas.exception.RegraDeNegocioExpection;
 import com.fquadros.minhasfinancas.model.Usuario;
 import com.fquadros.minhasfinancas.model.repository.UsuarioRepository;
 import com.fquadros.minhasfinancas.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
@@ -19,12 +23,24 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public Usuario autenticar(String email, String senha) {
-        return null;
+        Optional<Usuario> usuario = repository.findByEmail(email);
+
+        if (!usuario.isPresent()) {
+            throw new ErroAutenticacao("Usuario não encontrado para o email informado");
+        }
+
+        if (!usuario.get().getSenha().equals(senha)){
+            throw new ErroAutenticacao("Senha invalida.");
+        }
+
+        return usuario.get();
     }
 
     @Override
+    @Transactional
     public Usuario salvarUsuario(Usuario usuario) {
-        return null;
+        validarEmail(usuario.getEmail());
+        return repository.save(usuario);
     }
 
     @Override
