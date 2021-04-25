4f379af5b110858d534bbc589f5befd1200d1a7c
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-04-25 10:23:41 -0300

    Refactoring the User test Repository
    @SpringBootTest vai fazer que todo o contexto da aplicação springboot suba para realização de apenas uma classe, isso não é muito interessante, pois junto com o contexto vai vir classes de configuração, classe de serviços e etc, deixando mais demorado.
    
    podemos ver que da forma que fizemos o deveRetornarFalsoQuadnoNaoHouverUsuarioCadastradoComOEmail colocamos o repository.deleteAll(); para que o que ocorreu no cenario do teste anterior não interferisse neste
    
    @DataJpaTest cria uma instancia do banco de dados em memoria e ao finalizar a bateria de testes ela deleta da memoria.
    
    com essa annotation sempre que rodarmos um método com @Test ela abre uma transação e ao final ela efetua um roolback, desfazendo tudo o que foi feito na transação, e com isso diminuindo a intererencia com outros testes.
    
    @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) esta annotation é importante pois estamos dizendo para que ele não ignore as configurações que setamos no application-test.properties, se não ele vai sobreescrever as minhas configurações.
    
    entityManager é classe responsavel para efetuar as alteraçoes na base de dados (CRUD). O springboot ja use ele por debaixo dos panos, mas agora vamos utilizar ele diretamente para manipular o nosso cenario de testes.
    
    o que vamos usar é um entityManager especifico para testes, disponibilizado pelo JPA.

--- src/test/java/com/fquadros/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/fquadros/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,23 +5,29 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
 @ExtendWith({SpringExtension.class})
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UsuarioRepositoryTest {
 
     @Autowired
     UsuarioRepository repository;
 
+    @Autowired
+    TestEntityManager entityManager;
+
     @Test
     public void deveVerificarAExistenciaDeUmEmail(){
         //cenario
         Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-        repository.save(usuario);
+        entityManager.persist(usuario);
 
         //açao / execução
         boolean result = repository.existsByEmail("usuario@email.com");
@@ -33,7 +39,6 @@ class UsuarioRepositoryTest {
     @Test
     public void deveRetornarFalsoQuadnoNaoHouverUsuarioCadastradoComOEmail(){
         //cenario
-        repository.deleteAll();
 
         //acao
         boolean result = repository.existsByEmail("usuario@email.com");
