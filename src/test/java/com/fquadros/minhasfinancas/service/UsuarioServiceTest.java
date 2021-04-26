2af5a33dfc205934aa3975c95e8774c810f3fc84
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-04-26 19:21:53 -0300

    add Mocks in tests UsuarioService
    Mocks permitem que criamos um objeto/instancia fake de nossas classes onde podemos simular uma chamada de métodos e retorno de propriedades.
    
    vem junto com a dependência springboot starter test
    
    @MockBean
    UsuarioRepository repository;
    
    aqui usamos o repository como um bean gerenciado
    
    adicionando um mock na mão : repository = Mockito.mock(UsuarioRepository.class);

--- src/test/java/com/fquadros/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/fquadros/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,32 +1,36 @@
 package com.fquadros.minhasfinancas.service;
 
 import com.fquadros.minhasfinancas.exception.RegraDeNegocioExpection;
-import com.fquadros.minhasfinancas.model.Usuario;
 import com.fquadros.minhasfinancas.model.repository.UsuarioRepository;
+import com.fquadros.minhasfinancas.service.impl.UsuarioServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
 @ExtendWith({SpringExtension.class})
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-    @Autowired
     UsuarioService service;
 
-    @Autowired
+    @MockBean
     UsuarioRepository repository;
+
+    @BeforeEach
+    public void setUP(){
+        service =  new UsuarioServiceImpl(repository);
+    }
 
     @Test()
     public void deveValidarEmail(){
 
         //cenario
-        repository.deleteAll();
+        Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
 
         //ação
         service.validarEmail("email@email.com");
@@ -36,8 +40,7 @@ public class UsuarioServiceTest {
     public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado(){
 
         //cenario
-        Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
-        repository.save(usuario);
+        Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 
         //ação
         RegraDeNegocioExpection erro = Assertions.assertThrows(RegraDeNegocioExpection.class, () -> service.validarEmail("email@email.com"));
