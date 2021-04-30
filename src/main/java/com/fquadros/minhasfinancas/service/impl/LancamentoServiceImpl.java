1b8d8021fb9ab790ffb6dac3d2085d35c91b143a
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-04-30 18:31:55 -0300

    implement method of validated lançamentos

--- src/main/java/com/fquadros/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/fquadros/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.fquadros.minhasfinancas.service.impl;
 
+import com.fquadros.minhasfinancas.exception.RegraDeNegocioExpection;
 import com.fquadros.minhasfinancas.model.Lancamento;
 import com.fquadros.minhasfinancas.model.enums.StatusLancamento;
 import com.fquadros.minhasfinancas.model.repository.LancamentoRepository;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 
@@ -26,13 +28,16 @@ public class LancamentoServiceImpl implements LancamentoService {
     @Override
     @Transactional
     public Lancamento salvar(Lancamento lancamento) {
-         return repository.save(lancamento);
+        validar(lancamento);
+        lancamento.setStatus(StatusLancamento.PENDENTE);
+        return repository.save(lancamento);
     }
 
     @Override
     @Transactional
     public Lancamento atualizar(Lancamento lancamento) {
         Objects.requireNonNull(lancamento.getId());
+        validar(lancamento);
         return repository.save(lancamento);
     }
 
@@ -56,5 +61,33 @@ public class LancamentoServiceImpl implements LancamentoService {
     public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
         lancamento.setStatus(status);
         atualizar(lancamento);
+    }
+
+    @Override
+    public void validar(Lancamento lancamento) {
+
+        if (lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")){
+            throw new RegraDeNegocioExpection("Informe uma Descricao válida");
+        }
+
+        if (lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12 ){
+            throw  new RegraDeNegocioExpection("Informe um Mês valido");
+        }
+
+        if (lancamento.getAno() == null || lancamento.getAno().toString().length() != 4){
+            throw  new RegraDeNegocioExpection("Informe um ano valido");
+        }
+
+        if (lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null){
+            throw  new RegraDeNegocioExpection(("Informe um Usuario."));
+        }
+
+        if (lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1 ){
+            throw  new RegraDeNegocioExpection("Informe um valor válida");
+        }
+
+        if (lancamento.getTipo() == null){
+            throw new RegraDeNegocioExpection("Informe um tipo de lançamento");
+        }
     }
 }
